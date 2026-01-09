2e3784d15258de0fe26c01f70dc4af5674c5b4d2
Tenoo96 <ridik60302WP.PL>
2026-01-09 01:32:29 +0100

    Starting a new project. Creating a player-controlled bird that will fall and jump thanks to gravity. Implementing a simple wing flapping animation.

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,30 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 
 public class Bird : MonoBehaviour
 {
-    private const float JUMP_VELOCITY = 100f;
+    private const float JUMP_VELOCITY = 5f;
 
-    private Rigidbody2D rigidbodyBird;
-   
+    private Rigidbody2D birdRigidbody2D;
+    private InputSystemAction playerInputActions;
+    private Animator animator;
+
 
     private void Awake()
     {
-        rigidbodyBird = GetComponent<Rigidbody2D>();
+        birdRigidbody2D = GetComponent<Rigidbody2D>();
+
+        playerInputActions = new();
+        playerInputActions.Player.Enable();
+        playerInputActions.Player.Jump.performed += Jump;
     }
 
-    private void FixedUpdate()
+    //Jumps when the player appropriate key is pressed
+    private void Jump(InputAction.CallbackContext obj) 
     {
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        if (obj.performed)
         {
-            rigidbodyBird.linearVelocity = Vector2.up * JUMP_VELOCITY;
+            birdRigidbody2D.linearVelocity = Vector2.up * JUMP_VELOCITY;
         }
+
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    //Turns off gravity, flight animations and player input
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Dead");
+        playerInputActions.Player.Disable();
+
+        animator = GetComponentInChildren<Animator>();
+        animator.enabled = false;
+
+        birdRigidbody2D.simulated = false;
     }
+
 }
