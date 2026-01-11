a020082014546396ac177100ba4bb14537098038
Tenoo96 <ridik60302WP.PL>
2026-01-11 01:30:02 +0100

    I created a prefabricated column and then used it to create an obstacle. Each obstacle consists of two columns, each starting at the edge of the screen and ending with a gap between them. I also pre-finished the system for moving and generating additional obstacles.

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -1,96 +1,112 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using System.Data;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Level : MonoBehaviour
 {
-    [Header("Const")]
-    private const float COLUMN_WIDTH = 8.6f;
-    private const float SPEED_COLUMN = 5f;
-    private const int HALF_SIZE_SCREEN = 50;
-    private const int COLUMN_SPAWN_X_POS = 100;
-    private const int COLUMN_DESPAWN_X_POS = -100;
-    private const int COLUMN_COUNT = 10;
+    private const int HALF_SIZE_SCREEN = 5;
 
-    Transform obstacle;
-    private Vector3 columnSpawnPoint = new(COLUMN_SPAWN_X_POS, 0, 0);
-    private Vector3 columnDespawnPoint = new(COLUMN_DESPAWN_X_POS, 0, 0);
+    private const float COLUMN_WIDTH = 0.83f;
+    private const float COLUMN_SPEED = 5f;
+    private const int COLUMN_COUNT = 6;
+    private const int COLUMN_SPAWN_POS_X = 10;
+    private const int NEXT_COLUMN_SPAWN_POS_X = 5;
+    private const int COLUMN_REST_POS_X = -10;
 
-    private Queue<GameObject> columnHeadPool = new();
-    private Queue<GameObject> columnBodyPool = new();
+    [SerializeField] private Transform columnPref;
+    private List<Transform> obstacle = new();
+    private Queue<Transform> columnPool = new();
+    private Vector3 startPoint = new(10, 0, 0);
+    private Vector3 resetPoint = new(-10, 0, 0);
+
+
 
     private void Awake()
     {
         for (int i = 0; i < COLUMN_COUNT; i++)
         {
-            //Creating a column head pool
-            GameObject head = Instantiate(GameAssets.GetInstance.prefColumnHead);
-            head.transform.position = columnSpawnPoint;
-            head.SetActive(false);
-            columnHeadPool.Enqueue(head);
+            Transform column = Instantiate(columnPref);
+            column.transform.position = new(COLUMN_SPAWN_POS_X, 0, 0);
+            column.gameObject.SetActive(false);
+            columnPool.Enqueue(column);
 
-            //Creating a column body pool
-            GameObject body = Instantiate(GameAssets.GetInstance.prefColumnBody);
-            body.transform.position = columnSpawnPoint;
-            body.SetActive(false);
-            columnBodyPool.Enqueue(body);
         }
     }
-
     private void Start()
     {
-        CreatingObstacle(0, 40f, COLUMN_SPAWN_X_POS);
+        CreatingObstacle(0, 4f);
 
     }
-
-
-    private void CreatingObstacle(float gapY, float gapSize, float xPosition)
+    private void Update()
     {
-        float halfGap = gapSize * 0.5f;
-        Transform upColumn = SetColumnValue(xPosition, gapY + halfGap, false);
-        Transform downColumn = SetColumnValue(xPosition, gapY - halfGap, true);
-        StartCoroutine(ColumnMove(upColumn, downColumn));
-
-    }
-
-    private Transform SetColumnValue(float xPosition, float yPosition, bool onGround)
-    {
-        //Positioning column head
-        GameObject columnHead = columnHeadPool.Dequeue();
-        columnHead.transform.position = new(xPosition, yPosition, 0);
-        columnHead.transform.localScale = onGround ? Vector3.one : new(1, -1, 1);
-        columnHead.SetActive(true);
-        columnHeadPool.Enqueue(columnHead);
-
-        //Positioning column body and setting his height
-        GameObject columnBody = columnBodyPool.Dequeue();
-        columnBody.transform.position = new(xPosition, yPosition, 0);
-        SpriteRenderer colmnBodySpriteRenderer = columnBody.GetComponent<SpriteRenderer>();
-        columnBody.transform.localScale = onGround ? new(1, -1, 1) : Vector3.one;
-        float height = onGround ? yPosition + HALF_SIZE_SCREEN : HALF_SIZE_SCREEN - yPosition;
-        colmnBodySpriteRenderer.size = new(COLUMN_WIDTH, height);
-        columnBody.SetActive(true);
-        columnBodyPool.Enqueue(columnBody);
-        columnBody.transform.parent = columnHead.transform;
-
-        return columnHead.transform;
-    }
-
-    private IEnumerator ColumnMove(Transform uColumn, Transform dColumn)
-    {
-        while (uColumn.transform.position.x != COLUMN_DESPAWN_X_POS && dColumn.transform.position.x != COLUMN_DESPAWN_X_POS)
+        if (obstacle.Count > 0)
         {
-            float columnSpeed = SPEED_COLUMN * Time.deltaTime;
-          // Vector3 dirMove = Vector3.Lerp(columnSpawnPoint, columnDespawnPoint, columnSpeed);
-            uColumn.transform.position = Vector3.Lerp(columnSpawnPoint, columnDespawnPoint, columnSpeed);
-            dColumn.transform.position = Vector3.Lerp(columnSpawnPoint, columnDespawnPoint, columnSpeed);
+            List<Transform> objToMove = obstacle;
+
+
+            float speed = Time.deltaTime * COLUMN_SPEED;
+
+            foreach (Transform obj in objToMove)
+            {
+                startPoint = obj.position;
+                resetPoint = new(COLUMN_REST_POS_X, obj.position.y, obj.position.z);
+
+                obj.position = Vector3.MoveTowards(startPoint, resetPoint, speed);
+
+                if (obj.position == resetPoint)
+                {
+                    obj.gameObject.SetActive(false);
+                }
+
+            }
 
         }
 
-        uColumn.transform.GetChild(0).SetParent(null);
-        dColumn.transform.GetChild(0).SetParent(null);
+        if (obstacle[1].position.x == NEXT_COLUMN_SPAWN_POS_X)
+        {
+            CreatingObstacle(0, 4f);
+        }
+        else if (obstacle[1].position.x == COLUMN_REST_POS_X)
+        {
+            obstacle.Clear();
+            CreatingObstacle(0, 4f);
+        }
 
-        yield return null;
     }
+
+    private void CreatingObstacle(float gapY, float gapSize)
+    {
+        float halfGap = gapSize * 0.5f;
+        Transform upColumn = SetColumnParameters(true, new(COLUMN_SPAWN_POS_X, gapY + halfGap, 0));
+        Transform dawnColumn = SetColumnParameters(false, new(COLUMN_SPAWN_POS_X, gapY - halfGap, 0));
+
+        obstacle.Add(upColumn);
+        obstacle.Add(dawnColumn);
+
+    }
+
+    private Transform SetColumnParameters(bool inAir, Vector3 position)
+    {
+        Transform column = columnPool.Dequeue();
+        column.gameObject.SetActive(true);
+        column.transform.position = position;
+
+        //Get all Sprite Renderer components with children of the column and then we set the first component in the list as the column head and the second as its body
+        SpriteRenderer[] columnSpriteRenderer = column.GetComponentsInChildren<SpriteRenderer>();
+        SpriteRenderer headColumn = columnSpriteRenderer[0];
+        SpriteRenderer bodyColumn = columnSpriteRenderer[1];
+
+        //Setting length and orientation column
+        float height = inAir ? HALF_SIZE_SCREEN - position.y : position.y + HALF_SIZE_SCREEN;
+        bodyColumn.size = new(COLUMN_WIDTH, height);
+        bodyColumn.flipY = inAir;
+        headColumn.flipY = inAir;
+
+        columnPool.Enqueue(column);
+        return column;
+    }
+
 }
